b56a780b6c925e6f660b4b9e45c2071d521cd3ce
hussnain rehman <hussyrehman880@gmail.com>
2023-04-03 20:07:24 +0500

    added_extra_folder_to_gitignore

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,11 +1,12 @@
 import LoginPage from "../../page-objects/login-page";
 
 describe('Login', () => {
+
   it('should login successfully', () => {
     LoginPage.visit()
     LoginPage.fillEmail('your_username')
     LoginPage.fillPassword('your_password')
     LoginPage.submit()
-    cy.url().should('include', '/feed')
+    LoginPage.verifyHomePageIsLoaded()
   })
 })
